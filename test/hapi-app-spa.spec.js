050db09da1970f06663684dc849614f18bb05296
IndieForger <indieforger@gmail.com>
2016-03-25 06:55:54 +0000

    test refactoring

--- test/hapi-app-spa.spec.js
+++ test/hapi-app-spa.spec.js
@@ -4,6 +4,7 @@ const Code = require('code');
 const Lab = require('lab');
 const Spa = require('..');
 const Path = require('path');
+const Forger = require('forger');
 
 const lab = exports.lab = Lab.script();
 const describe = lab.describe;
@@ -14,66 +15,104 @@ const beforeEach = lab.beforeEach;
 
 describe('hapi-app-spa', () => {
 
-	let server;
+	describe('plugin', () => {
 
-	beforeEach((done) => {
-		server = new Hapi.Server();
-		server.connection();
-		done();
+		it('should register', (done) => {
+			const server = new Hapi.Server();
+			server.connection();
+
+			server.register(require('../lib/hapi-app-spa'), (err) => {
+				expect(err).to.not.exist();
+
+				server.initialize((err) => {
+					expect(err).to.not.exist();
+					done();
+				});
+			});
+		});
 	});
 
-	it('should register', (done) => {
-		server.register(require('../lib/hapi-app-spa'), (err) => {
-			expect(err).to.not.exist();
+	describe('default options', () => {
 
-			server.initialize((err) => {
-				expect(err).to.not.exist();
+		let server;
+
+		beforeEach((done) => {
+			server = new Hapi.Server();
+			server.connection();
+			server.register({
+				register: require('../lib/hapi-app-spa'),
+				options: {
+					relativeTo: Path.join(__dirname, './public')
+				}
+			}, (err) => {
+				server.initialize((err) => {
+					done(err);
+				});
+			});
+		});
+
+		it('should serve index for any non-file url', (done) => {
+			server.inject('/custom/path', (res) => {
+				expect(res.statusCode).to.equal(200);
+				expect(res.result).to.equal('<html><head><title>test</title></head><body><p>test</p></body></html>');
 				done();
 			});
 		});
 
-	});
+		it('should respond with 404 for missing resources', (done) => {
 
-	it('should respond with 404', (done) => {
+			Forger.sequence(
+				(next) => injectRequest('/css/missing.css', next),
+				(next) => injectRequest('/tpl/missing.tpl.html', next),
+				(next) => injectRequest('/js/missing.js', next),
+				(next) => injectRequest('/file/missing.doc', next),
+				(next) => injectRequest('/img/missing.jpg', next)
+			).then(() => {
+				done();
+			}).catch((err) => {
+				done(err);
+			});
 
-		server.register(require('../lib/hapi-app-spa'), (err) => {
-			expect(err).to.not.exist();
-
-			server.initialize((err) => {
-				expect(err).to.not.exist();
-
-				server.inject('/', (res) => {
+			function injectRequest(url, next) {
+				server.inject(url, (res) => {
 					expect(res.statusCode).to.equal(404);
-					done();
+					next(null);
 				});
-			});
+			}
 		});
+
+
 	});
 
-	it('should respond with file', (done) => {
-		const plugin = {
-			register: require('../lib/hapi-app-spa'),
-			options: {
-				relativeTo: Path.join(__dirname, '../')
-			}
-		};
+	describe('custom options', () => {
 
-		server.register(plugin, (err) => {
-			expect(err).to.not.exist();
+		it('should respond with file', (done) => {
 
-			server.initialize((err) => {
+			const server = new Hapi.Server();
+			const plugin = {
+				register: require('../lib/hapi-app-spa'),
+				options: {
+					relativeTo: Path.join(__dirname, '../')
+				}
+			};
+
+			server.connection();
+			server.register(plugin, (err) => {
 				expect(err).to.not.exist();
 
-				server.inject('/package.json', (res) => {
-					expect(res.statusCode).to.equal(200);
+				server.initialize((err) => {
+					expect(err).to.not.exist();
 
-					var packageJson = JSON.parse(res.result);
-					expect(packageJson.version).to.equal(require('../package.json').version);
-					done();
+					server.inject('/package.json', (res) => {
+						expect(res.statusCode).to.equal(200);
+
+						var packageJson = JSON.parse(res.result);
+						expect(packageJson.version).to.equal(require('../package.json').version);
+						done();
+					});
 				});
-
 			});
 		});
 	});
-
 });
+
